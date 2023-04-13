f806b989772ba2c0f8d2fb12cfec39ce8002d877
Armando Del Rio <39106560+adelriorz@users.noreply.github.com>
2023-04-13 00:54:40 -0600

    Update Credentials.js

--- frontend/src/credentials/Credentials.js
+++ frontend/src/credentials/Credentials.js
@@ -1,3 +1,3 @@
-// const clientId = "1079690044277-den4kibmv1t7i58ohccqea1v0p3ehi6a.apps.googleusercontent.com";
+// const clientId = "";
 
-// const clientSecret="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";
+// const clientSecret="";
